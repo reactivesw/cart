f9ee0f030a6845132ed1bee4d6c0638ebbafb6e3
umasuo <liuquan89@gmail.com>
2017-03-21 16:04:55 +0800

    refactor scripts

--- src/main/java/io/reactivesw/cart/application/model/ShippingInfoView.java
+++ src/main/java/io/reactivesw/cart/application/model/ShippingInfoView.java
@@ -2,39 +2,25 @@ package io.reactivesw.cart.application.model;
 
 import io.reactivesw.model.Money;
 import io.reactivesw.model.Reference;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * Created by umasuo on 16/11/17.
  */
 @Data
-@ApiModel
 public class ShippingInfoView {
 
-  @ApiModelProperty(value = "Reference to a ShippingMethod, Not set if custom shipping method is " +
-      "used.")
   private Reference shippingMethod;
 
-  @ApiModelProperty(required = true)
   private String shippingMethodName;
 
-  @ApiModelProperty(value = "Determined based on the ShippingRateView and the sum of LineItemView prices.",
-      required = true)
   private Money price;
 
-  @ApiModelProperty(value = "The shipping rate used to determine the price.", required = true)
   private ShippingRateView shippingRate;
 
-  @ApiModelProperty(value = "Set once the taxRate is set.")
   private TaxedItemPriceView taxedPrice;
 
-  @ApiModelProperty(value = "Will be set automatically in the Platform TaxMode once the shipping " +
-      "address is set is set. For the External tax mode the tax rate has to be set explicitly " +
-      "with the ExternalTaxRateDraft.")
   private TaxRateView taxRate;
 
-  @ApiModelProperty(value = "Reference to a TaxCategory")
   private Reference taxCategory;
 }
