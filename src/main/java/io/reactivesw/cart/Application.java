0073250a276f421c309e19c71d06067566f79b3c
kaka <liuquan89@gmail.com>
2017-04-17 17:15:27 +0800

    Add event support: add customer signin event consumer.
    Add event support: add customer signin event consumer.

--- src/main/java/io/reactivesw/cart/Application.java
+++ src/main/java/io/reactivesw/cart/Application.java
@@ -2,11 +2,13 @@ package io.reactivesw.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * Created by umasuo on 17/2/9.
+ * Micro service entrance.
  */
 @SpringBootApplication(scanBasePackages = "io.reactivesw")
+@EnableScheduling
 public class Application {
 
   public static void main(String[] args) {
