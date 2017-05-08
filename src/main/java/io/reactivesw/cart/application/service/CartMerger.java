40564123f75c94829ff7cfd8d13feba0a40ad5d3
kaka <liuquan89@gmail.com>
2017-05-08 18:09:08 +0800

    Fixbug in cart merge
    Fixbug in cart merge

--- src/main/java/io/reactivesw/cart/application/service/CartMerger.java
+++ src/main/java/io/reactivesw/cart/application/service/CartMerger.java
@@ -48,8 +48,8 @@ public class CartMerger {
 
     Cart anonymousCart = cartService.getCartByAnonymousId(anonymousId);
     if (anonymousCart == null
-        || anonymousCart.getLineItems() != null
-        || anonymousCart.getLineItems().size() > 0) {
+        || anonymousCart.getLineItems() == null
+        || anonymousCart.getLineItems().isEmpty()) {
       LOG.error("Merge cart failed for anonymous cart not exist. customerId: {}, anonymousId: "
           + "{}.", customerId, anonymousId);
 
