ff89bffcdd7ab8305171d400b505cf9cbb18c8f1
umasuo <liuquan89@gmail.com>
2017-05-08 14:00:58 +0800

    finish test

--- src/main/java/io/reactivesw/cart/Application.java
+++ src/main/java/io/reactivesw/cart/Application.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * Micro service entrance.
  */
 @SpringBootApplication(scanBasePackages = "io.reactivesw")
-//@EnableScheduling
+@EnableScheduling
 public class Application {
 
   public static void main(String[] args) {
